607ba0fef0d378060896466ee150502b4aa5c0f9
YuamLu <luyuam0@gmail.com>
2022-06-20 00:35:43 +0800

    0

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function productObjClick(){
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
 function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const accounts = Promise.resolve(ethereum.request({ method: 'eth_requestAccounts' }));
   const account = accounts[0];
   showAccount.innerHTML = account;
 }
