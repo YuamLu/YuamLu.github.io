20458d8829460eafd5667f5b8aaf5237c8ef36bc
YuamLu <luyuam0@gmail.com>
2022-06-19 22:48:27 +0800

    commit

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function productObjClick(){
     document.getElementById("showHidden").style.display = "block";
 }
+
+
+const ethereumButton = document.querySelector('.button5');
+ethereumButton.addEventListener('click', () => {
+
+  ethereum.request({ method: 'eth_requestAccounts' });
+});
