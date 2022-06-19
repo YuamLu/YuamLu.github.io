416475a1be8151e7d2a74d30b8d804883d14a9ce
YuamLu <luyuam0@gmail.com>
2022-06-20 00:02:57 +0800

    0

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function productObjClick(){
     document.getElementById("showHidden").style.display = "block";
 }
+
+
+
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
+function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}
